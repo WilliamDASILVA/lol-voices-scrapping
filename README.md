2f0866d7dd82617d39df0748b9d2c6737e9c65bc
William DA SILVA <william.da.silva@outlook.com>
2023-11-18 00:05:34 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# LoL Voices Scrapping
---
+++ index.js
@@ -0,0 +1,203 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
+
+const champions = [
+  "Aatrox",
+  "Ahri",
+  "Akali",
+  "Alistar",
+  "Amumu",
+  "Anivia",
+  "Annie",
+  "Aphelios",
+  "Ashe",
+  "Aurelion Sol",
+  "Azir",
+  "Bard",
+  "Blitzcrank",
+  "Brand",
+  "Braum",
+  "Caitlyn",
+  "Camille",
+  "Cassiopeia",
+  "Cho'Gath",
+  "Corki",
+  "Darius",
+  "Diana",
+  "Dr. Mundo",
+  "Draven",
+  "Ekko",
+  "Elise",
+  "Evelynn",
+  "Ezreal",
+  "Fiddlesticks",
+  "Fiora",
+  "Fizz",
+  "Galio",
+  "Gangplank",
+  "Garen",
+  "Gnar",
+  "Gragas",
+  "Graves",
+  "Gwen",
+  "Hecarim",
+  "Heimerdinger",
+  "Illaoi",
+  "Irelia",
+  "Ivern",
+  "Janna",
+  "Jarvan IV",
+  "Jax",
+  "Jayce",
+  "Jhin",
+  "Jinx",
+  "Kai'Sa",
+  "Kalista",
+  "Karma",
+  "Karthus",
+  "Kassadin",
+  "Katarina",
+  "Kayle",
+  "Kayn",
+  "Kennen",
+  "Kha'Zix",
+  "Kindred",
+  "Kled",
+  "Kog'Maw",
+  "LeBlanc",
+  "Lee Sin",
+  "Leona",
+  "Lillia",
+  "Lissandra",
+  "Lucian",
+  "Lulu",
+  "Lux",
+  "Malphite",
+  "Malzahar",
+  "Maokai",
+  "Master Yi",
+  "Miss Fortune",
+  "Mordekaiser",
+  "Morgana",
+  "Nami",
+  "Nasus",
+  "Nautilus",
+  "Neeko",
+  "Nidalee",
+  "Nocturne",
+  "Nunu & Willump",
+  "Olaf",
+  "Orianna",
+  "Ornn",
+  "Pantheon",
+  "Poppy",
+  "Pyke",
+  "Qiyana",
+  "Quinn",
+  "Rakan",
+  "Rammus",
+  "Rek'Sai",
+  "Rell",
+  "Renekton",
+  "Rengar",
+  "Riven",
+  "Rumble",
+  "Ryze",
+  "Samira",
+  "Sejuani",
+  "Senna",
+  "Seraphine",
+  "Sett",
+  "Shaco",
+  "Shen",
+  "Shyvana",
+  "Singed",
+  "Sion",
+  "Sivir",
+  "Skarner",
+  "Sona",
+  "Soraka",
+  "Swain",
+  "Sylas",
+  "Syndra",
+  "Tahm Kench",
+  "Taliyah",
+  "Talon",
+  "Taric",
+  "Teemo",
+  "Thresh",
+  "Tristana",
+  "Trundle",
+  "Tryndamere",
+  "Twisted Fate",
+  "Twitch",
+  "Udyr",
+  "Urgot",
+  "Varus",
+  "Vayne",
+  "Veigar",
+  "Vel'Koz",
+  "Vi",
+  "Viego",
+  "Viktor",
+  "Vladimir",
+  "Volibear",
+  "Warwick",
+  "Wukong",
+  "Xayah",
+  "Xerath",
+  "Xin Zhao",
+  "Yasuo",
+  "Yone",
+  "Yorick",
+  "Yuumi",
+  "Zac",
+  "Zed",
+  "Ziggs",
+  "Zilean",
+  "Zoe",
+  "Zyra",
+];
+const baseURL = "https://leagueoflegends.fandom.com/wiki";
+
+const downloadAudio = async (url, filename) => {
+  const response = await axios({
+    method: "GET",
+    url: url,
+    responseType: "stream",
+  });
+
+  response.data.pipe(fs.createWriteStream(`downloads/${filename}`));
+};
+
+const fetchAndParsePage = async (url, championName) => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    $("audio source").each((index, element) => {
+      const audioUrl = $(element).attr("src");
+      const filename = `${championName}_${path.basename(audioUrl)}.ogg`;
+      console.log(`=== Downloading ${filename}...`);
+
+      downloadAudio(audioUrl, filename);
+    });
+  } catch (error) {
+    console.error(
+      `Error fetching or parsing the page for ${championName}:`,
+      error
+    );
+  }
+};
+
+const main = async () => {
+  for (const champion of champions) {
+    console.log(`Fetching ${champion}...`);
+    const championUrl = `${baseURL}/${champion}/LoL/Audio`;
+    await fetchAndParsePage(championUrl, champion);
+  }
+};
+
+main();
